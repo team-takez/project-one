1114f7f51d57b5b550ecdb5a7ed9f286344db873
Aaron Kennebrew <aaronk@Aarons-MacBook-Pro.local>
2022-12-01 21:05:53 -0600

    added second fetch to retrieve cities

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,10 @@ let searchForm = document.querySelector("#search-form");
 let searchInput = document.querySelector("#searchInput");
 let restaurantCard = document.querySelector("#restaurant-card");
 let dogBreeds = document.querySelector("#dog-breeds");
+// let cuisineType = document.querySelector("");
+// let location = document.querySelector("");
+// let priceRange = document.querySelector("");
+
 //functions
 function init() {
   fetch("https://dog.ceo/api/breeds/list/all")
@@ -22,29 +26,44 @@ function init() {
 function handleFormSubmit(event) {
   event.preventDefault();
   restaurantCard.innerHTML = "";
-
-  let requestUrl = `https://worldwide-restaurants.p.rapidapi.com/search?language=en_US&limit=30&location_id=35805&currency=USD`;
-  fetch(requestUrl, {
+  let locationRequestUrl = "https://worldwide-restaurants.p.rapidapi.com/typeahead?q=Atlanta&language=en_US&currency=USD";
+  fetch(locationRequestUrl, {
     method: "POST",
     headers: {
       "content-type": "application/x-www-form-urlencoded",
-      "X-RapidAPI-Key": "77c49fa958msh4ad3388de6c9df6p1e0636jsn0b5317d9f072",
+      "X-RapidAPI-Key": "bc1f799ce0mshe02c6b2a2eb17b3p1a13a9jsn5c26cb1c4ec5",
       "X-RapidAPI-Host": "worldwide-restaurants.p.rapidapi.com",
     },
   })
     .then(function (response) {
       return response.json();
     })
-    .then(function (data) {
-      console.log(data);
-        data.results.data.forEach((restaurant, index) => {
-          if (index < 10) {
-            let divTag = document.createElement("div");
-            divTag.setAttribute("class", "p-2");
-            divTag.innerHTML = `<h2> ${restaurant.name} </h2>`
-            divTag.innerHTML +=  `<img class="" src="${restaurant.photo.images.original.url}">`;
-            restaurantCard.append(divTag);
-          }
+    .then(function (location) {
+let locationID = location.results.data[0].result_object.location_id;
+console.log(locationID)
+      let requestUrl = `https://worldwide-restaurants.p.rapidapi.com/search?language=en_US&limit=30&location_id=${locationID}&currency=USD`;
+      fetch(requestUrl, {
+        method: "POST",
+        headers: {
+          "content-type": "application/x-www-form-urlencoded",
+          "X-RapidAPI-Key": "bc1f799ce0mshe02c6b2a2eb17b3p1a13a9jsn5c26cb1c4ec5",
+          "X-RapidAPI-Host": "worldwide-restaurants.p.rapidapi.com",
+        },
+      })
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data);
+          data.results.data.forEach((restaurant, index) => {
+            if (index < 10) {
+              let divTag = document.createElement("div");
+              divTag.setAttribute("class", "p-2");
+              divTag.innerHTML = `<h2> ${restaurant.name} </h2>`;
+              divTag.innerHTML += `<img class="" src="${restaurant.photo.images.original.url}">`;
+              restaurantCard.append(divTag);
+            }
+          });
         });
     });
 }
